3a613a856db82f9762e748a75a730fe136512082
William Convertino <williamconvertino@gmail.com>
2025-06-04 18:30:11 -0400

    Updated device settings

--- icl/scripts/run_experiment.sh
+++ icl/scripts/run_experiment.sh
@@ -1,7 +1,8 @@
 # Lightning Settings
-# CUDA_VISIBLE_DEVICES=1,2,3 \
+# CUDA_VISIBLE_DEVICES=3 \
 OMP_NUM_THREADS=16 \
 torchrun --nproc_per_node=1 \
 train.py \
 --strategy auto \
+--devices 1 \
 "$@"
