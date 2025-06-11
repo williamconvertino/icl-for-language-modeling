966fd9bfc49d3d78ca9412d51b1e66d991ccf481
William Convertino <williamconvertino@gmail.com>
2025-06-11 10:53:26 -0400

    Updated node list

--- scripts/multi_gpu_run.sh
+++ scripts/multi_gpu_run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=icl-language
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=4
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=128GB
+#SBATCH --mem-per-cpu=64G
+#SBATCH --gres=gpu:1
 #SBATCH --time=48:00:00
-#SBATCH --partition=gpu-common
+#SBATCH --partition=scavenger-gpu
+#SBATCH --exclude=dcc-youlab-gpu-28,dcc-gehmlab-gpu-56
+#SBATCH --nodelist=dcc-allenlab-gpu-[01-04],dcc-allenlab-gpu-[05-12],dcc-majoroslab-gpu-[01-08],dcc-yaolab-gpu-[01-08],dcc-wengerlab-gpu-01,dcc-engelhardlab-gpu-[01-04],dcc-motesa-gpu-[01-04],dcc-pbenfeylab-gpu-[01-04],dcc-vossenlab-gpu-[01-04],dcc-youlab-gpu-[01-56],dcc-mastatlab-gpu-01,dcc-viplab-gpu-01,dcc-youlab-gpu-57
+#HIGH VRAM --nodelist=dcc-allenlab-gpu-[01-04],dcc-allenlab-gpu-[05-12],dcc-majoroslab-gpu-[01-08]
+#SBATCH --requeue
 #SBATCH --output=logs/%x-%j.out
-
+#SBATCH --error=logs/%x-%j.err
 source ~/.bashrc
 conda activate icl
 
--- scripts/single_gpu_run.sh
+++ scripts/single_gpu_run.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 #SBATCH --job-name=icl-language
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=64GB
+#SBATCH --mem-per-cpu=64G
+#SBATCH --gres=gpu:1
 #SBATCH --time=48:00:00
-#SBATCH --partition=gpu-common
+#SBATCH --partition=scavenger-gpu
+#SBATCH --exclude=dcc-youlab-gpu-28,dcc-gehmlab-gpu-56
+#SBATCH --nodelist=dcc-allenlab-gpu-[01-04],dcc-allenlab-gpu-[05-12],dcc-majoroslab-gpu-[01-08],dcc-yaolab-gpu-[01-08],dcc-wengerlab-gpu-01,dcc-engelhardlab-gpu-[01-04],dcc-motesa-gpu-[01-04],dcc-pbenfeylab-gpu-[01-04],dcc-vossenlab-gpu-[01-04],dcc-youlab-gpu-[01-56],dcc-mastatlab-gpu-01,dcc-viplab-gpu-01,dcc-youlab-gpu-57
+#HIGH VRAM --nodelist=dcc-allenlab-gpu-[01-04],dcc-allenlab-gpu-[05-12],dcc-majoroslab-gpu-[01-08]
+#SBATCH --requeue
 #SBATCH --output=logs/%x-%j.out
+#SBATCH --error=logs/%x-%j.err
 
+set -e
 source ~/.bashrc
 conda activate icl
 
-# Use the first device, unless specified
+# Default to using GPU 0 unless overridden
 DEVICES="0"
-
 for arg in "$@"; do
   if [[ "$arg" == --device=* ]]; then
     DEVICES="${arg#*=}"
@@ -22,7 +27,6 @@ for arg in "$@"; do
 done
 
 export CUDA_VISIBLE_DEVICES=$DEVICES
-
 export OMP_NUM_THREADS=10
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
