8a9e8b542681d9944c0ad517dcdbd8ec71e31e77
William Convertino <williamconvertino@gmail.com>
2025-06-26 14:46:13 -0400

    Added goodwiki dataset

---
+++ src/data/goodwiki_dataset.py
@@ -0,0 +1,34 @@
+import os
+from datasets import load_dataset, concatenate_datasets
+from .disk_dataset import DiskDataset, DATASET_DIR
+
+HUGGINGFACE_PATH = "euirim/goodwiki"
+
+class GoodWikiDataset(DiskDataset):
+    
+    def __init__(self, split, tokenizer, context_size, allow_overlap=True):
+    
+        file_path = f"{DATASET_DIR}/goodwiki/{split}.bin"    
+        
+        if not os.path.exists(file_path):
+
+            print(f"Creating GoodWiki [{split}] dataset files...")
+            
+            dataset = load_dataset(HUGGINGFACE_PATH, cache_dir=f"{DATASET_DIR}/raw", split="train")
+            
+            val_data = train_data.select(range(2_000)) # 2k val
+            test_data = train_data.select(range(2_000, 4_000)) # 2k test
+            train_data = train_data.select(range(4_000, len(train_data))) # 40k train     
+
+            DiskDataset.generate_data_file(train_data, f"{DATASET_DIR}/goodwiki/train.bin", tokenizer)
+            DiskDataset.generate_data_file(test_data, f"{DATASET_DIR}/goodwiki/test.bin", tokenizer)
+            DiskDataset.generate_data_file(val_data, f"{DATASET_DIR}/goodwiki/val.bin", tokenizer)
+    
+        super().__init__(file_path, tokenizer, context_size, allow_overlap=allow_overlap)
+
+    def get_splits(tokenizer, max_seq_len):
+        return {
+            "train": GoodWikiDataset("train", tokenizer, max_seq_len, allow_overlap=True),
+            "val": GoodWikiDataset("val", tokenizer, max_seq_len, allow_overlap=False),
+            "test": GoodWikiDataset("test", tokenizer, max_seq_len, allow_overlap=False)
+        }
